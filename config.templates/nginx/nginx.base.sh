a202d943489a36dfcd37e8a81552d58b980a6484
Levani Ramazashvili <levani@MacBook-Air-3.local>
2024-10-22 13:16:56 +0400

    Fix nginx template

--- config.templates/nginx/nginx.base.sh
+++ config.templates/nginx/nginx.base.sh
@@ -45,8 +45,7 @@ $NGINX_TECT_STATUS
 	location ~* ".+\.(?:ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|rss|css|swf|js|atom|jpe?g|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav|bmp|rtf)$" {
 		access_log                off;
 		log_not_found             off;
-		expires                   max;
-		expires 30d;
+		expires                   30d;
 		add_header Cache-Control "public, no-transform";
 	}
 
